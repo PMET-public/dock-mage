dcc2d0e67331794342f2ae1d63f172b803855888
Keith Bentrup <keith@keithbentrup.com>
2017-11-14 15:12:05 -0500

    updates

--- scripts/environment-setup.sh
+++ scripts/environment-setup.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-
 SCRIPTS_DIR=$( cd $(dirname $0) ; pwd -P )
-
 
 for ini in "${SCRIPTS_DIR}"/../env/*.ini; do
   # output vars so they can exported in calling shell
@@ -12,7 +10,6 @@ for ini in "${SCRIPTS_DIR}"/../env/*.ini; do
 done
 
 echo XDEBUG_REMOTE_HOST=$(docker run --rm --privileged --pid=host debian:stable-slim nsenter -t 1 -m -u -n -i sh -c "ip route|awk '/default/{print \$3}'")
-
 
 echo MAGENTO_CLOUD_RELATIONSHIPS=$(cat "${SCRIPTS_DIR}"/../env/MAGENTO_CLOUD_RELATIONSHIPS.yaml |
   python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' |
@@ -26,3 +23,12 @@ echo MAGENTO_CLOUD_ROUTES=$(cat "${SCRIPTS_DIR}"/../../../../.magento/routes.yam
   perl -pe 's/{default}/exists $ENV{"MAGENTO_HOSTNAME"} ? $ENV{"MAGENTO_HOSTNAME"} : die "HOSTNAME undefined"/ge' |
   python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' |
   base64)
+
+
+if docker ps --format "{{ .Names }}" | egrep -q "^${MAGENTO_HOSTNAME}\$"; then
+  >&2 echo -e "\nContainer with name ${MAGENTO_HOSTNAME} already exists. If you want to create a new container, use:\n\n\e[33mexport MAGENTO_HOSTNAME=www.sample.com\e[0m\n"
+fi
+
+if [ ! -z "$(docker-compose ps -q)" ]; then
+  >&2 echo -e "\nProject with name ${COMPOSE_PROJECT_NAME} already exists. If you want to create a new project, use:\n\n\e[33mexport COMPOSE_PROJECT_NAME=myprefix\e[0m\n"
+fi
