94e2baba60594a6405ce7a58b748244303ed1a91
Keith Bentrup <keith@keithbentrup.com>
2017-11-14 22:41:13 -0500

    fix hostname check

--- scripts/environment-setup.sh
+++ scripts/environment-setup.sh
@@ -25,7 +25,7 @@ echo MAGENTO_CLOUD_ROUTES=$(cat "${SCRIPTS_DIR}"/../../../../.magento/routes.yam
   base64)
 
 
-if [ ! -z "$(docker ps -a --filter 'name=^/${MAGENTO_HOSTNAME}\$')" ]; then
+if [ ! -z "$(docker ps -qa --filter 'name=^/${MAGENTO_HOSTNAME}\$')" ]; then
   >&2 echo -e "\nContainer with name ${MAGENTO_HOSTNAME} already exists. If you want to create a new container, use:\n\n\e[33mexport MAGENTO_HOSTNAME=www.sample.com\e[0m"
 fi
 
