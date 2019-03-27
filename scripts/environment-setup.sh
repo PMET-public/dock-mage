354508f173ef714e8db3fea793020b6cfa016004
Keith Bentrup <keith@keithbentrup.com>
2019-03-26 20:00:14 -0400

    change how env vars are loaded

--- scripts/environment-setup.sh
+++ scripts/environment-setup.sh
@@ -3,11 +3,28 @@
 # stop on errors
 set -e
 
+# show debug info if debug is set
+if [[ ! -z "${debug}" ]]; then
+  set -x
+fi
+
 SCRIPTS_DIR=$( cd $(dirname $0) ; pwd -P )
 
-if ! ( pip freeze | grep -q PyYAML) ; then
-  echo "Please install PyYAML before continuing ..." && exit 1
+full_path=$(cd "${SCRIPTS_DIR}"/../../../.. && pwd -P)
+dir_name=$(basename "${full_path}")
+if [[ 
+  "${COMPOSE_PROJECT_NAME}" != "${dir_name}" ||
+  "${COMPOSE_FILE}" != "${full_path}/vendor/magentoese/dock-mage/docker-compose.yml" ||
+  "${COMPOSE_PROJECT_NAME}" != "${mhost}" 
+]]; then
+  echo -e "Before continuing, please run:\n\033[32mexport COMPOSE_PROJECT_NAME=\"${dir_name}\" \\ \nCOMPOSE_FILE=\"${full_path}/vendor/magentoese/dock-mage/docker-compose.yml\" \\ \nmhost=\"${dir_name}\"\033[0m"
+  exit
 fi
+exit
+if ! ( pip3 freeze | grep -q PyYAML) ; then
+  echo "Please ensure you have python3, pip3, and PyYAML before continuing ..." && exit 1
+fi
+
 
 # if a value is echoed, it's for use in the calling env
 # if a value is exported, it's to be used in the docker-compose.yml
@@ -27,10 +44,6 @@ for ini in "${SCRIPTS_DIR}"/../env/*.ini; do
   export $(interpolate_ini $ini)
 done
 
-export XDEBUG_REMOTE_HOST=$(docker run --rm --privileged --pid=host debian:stable-slim nsenter -t 1 -m -u -n -i sh -c "ip route|awk '/default/{print \$3}'")
-echo COMPOSE_PROJECT_NAME=$( [[ -n "${COMPOSE_PROJECT_NAME}" ]] && echo "${COMPOSE_PROJECT_NAME}" || basename $(cd "${SCRIPTS_DIR}"/../../../.. && pwd) )
-export MAGENTO_HOSTNAME=$( [[ -n "${COMPOSE_PROJECT_NAME}" ]] && echo "${COMPOSE_PROJECT_NAME}" || basename $(cd "${SCRIPTS_DIR}"/../../../.. && pwd) )
-echo "MAGENTO_HOSTNAME=${MAGENTO_HOSTNAME}"
 
 
 # write config files with exported env vars if defined
@@ -42,22 +55,22 @@ echo MAGENTO_CLOUD_TREE_ID=$(cd "${SCRIPTS_DIR}/../../../.."; git rev-parse HEAD
 echo MAGENTO_CLOUD_BRANCH=$(cd "${SCRIPTS_DIR}/../../../.."; git rev-parse --abbrev-ref HEAD)
 
 echo MAGENTO_CLOUD_RELATIONSHIPS=$(cat "${SCRIPTS_DIR}"/../env/MAGENTO_CLOUD_RELATIONSHIPS.yaml |
-  python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' |
+  python3 -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' |
   base64)
 
 echo MAGENTO_CLOUD_VARIABLES=$(cat "${SCRIPTS_DIR}"/../env/MAGENTO_CLOUD_VARIABLES.yaml |
-  python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' |
+  python3 -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' |
   base64)
 
 # need to append original url b/c deploy scripts expecting this value in MAGENTO_CLOUD_ROUTES but it's not a recognized key in .magento/routes.yaml
 tmp_yaml=$(cat "${SCRIPTS_DIR}"/../../../../.magento/routes.yaml <(echo '    original_url: "https://{default}/"'))
 echo MAGENTO_CLOUD_ROUTES=$(echo "${tmp_yaml}" |
-  perl -pe 's/{default}\/":/exists $ENV{"MAGENTO_HOSTNAME"} ? $ENV{"MAGENTO_HOSTNAME"}."\/\":" : die "HOSTNAME undefined"/ge' |
-  python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' |
+  perl -pe 's/{default}\/":/exists $ENV{"mhost"} ? $ENV{"mhost"}."\/\":" : die "HOSTNAME undefined"/ge' |
+  python3 -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' |
   base64)
 
-if [ ! -z "$(docker ps -qa --filter "name=^/${MAGENTO_HOSTNAME}\$")" ]; then
-  >&2 echo -e "\nContainer with name ${MAGENTO_HOSTNAME} already exists. If you want to create a new container, use:\n\n\033[33mexport MAGENTO_HOSTNAME=www.sample.com\033[0m"
+if [ ! -z "$(docker ps -qa --filter "name=^/${mhost}\$")" ]; then
+  >&2 echo -e "\nContainer with name ${mhost} already exists. If you want to create a new container, use:\n\n\033[33mexport mhost=www.sample.com\033[0m"
 fi
 
 if [ ! -z "$(docker-compose ps -q 2>/dev/null)" ]; then
